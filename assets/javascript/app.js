54ab61589a13a057e34a49a025f4a52c06d4893b
andre2992 <andrebrown2929@gmail.com>
2018-05-17 14:54:54 -0400

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
 
-var topics = ['nike', 'puma', 'adidas', 'converse'];
-var limit =10;
+    var topics = ['nike', 'puma', 'adidas', 'converse'];
+    var limit = 10;
+
+    function buttons() {
+        $('#button').empty()
+
+        for (var i = 0; i < Topics.length; i++) {
+            var button = $("<button>");
+            btn.addClass("topic");
+            btn.attr("data-topic", Topics[i]);
+            btn.text(Topics[i]);
+            $("#button-section").prepend(btn);
+            $("#gif").val('');
+
+        }
+    };
 });
