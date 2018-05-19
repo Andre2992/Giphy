aa4c39cb0a76ca6c75caec5eff37a93a40a7ee80
andre2992 <andrebrown2929@gmail.com>
2018-05-19 12:35:21 -0400

    appended new data

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,69 +1,78 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-
-    var brands = ['nike', 'puma', 'adidas', 'converse'];
-    var limit = 10;
+    var brands = ["nike", "puma", "converse"];
+    var limit= 10;
 
     function displayGifs() {
         $('.gifsAppearHere').empty()
         var brand = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            brand + "&api_key=dc6zaTOxFJmzC&limit=" + limit;
+            brand + "&api_key=qzU8Uj6qG1oQC27IJD500uNmiIHjbxkG&limit=" + limit;
 
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function (response) {
-                var results = response.data;
+        // Creates AJAX call for the specific button being clicked
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var results = response.data;
 
-                for (var i = 0; i < results.length; i++) {
-                    var gifDiv = $('<div class="myGifs">');
-    
-                    var rating = results[i].rating;
-    
-                    var rat = $('<div class="rating">').text("Rating: " + rating);
-    
-                    var gifImage = $("<img>");
-                    gifImage.attr("src", results[i].images.fixed_height.url);
-    
-                    gifDiv.append(gifImage);
-                    gifDiv.append(rat);
-    
-                    $(".gifsAppearHere").append(gifDiv);
-                    console.log(results[i].images.fixed_height.url)
-                }
-            });
-        }
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $('<div class="myGifs">');
 
-        function renderButtons() {
-            $("#buttons-view").empty();
-            for (var i = 0; i < brands.length; i++) {
-                var a = $("<button>");
-                a.addClass("brand");
-                a.attr("data-name", brands[i]);
-                a.text(brands[i]);
-                $("#buttons-view").append(a);
+                var rating = results[i].rating;
+
+                var rate = $('<div class="rating">').text("Rating: " + rating);
+
+                var gifImage = $("<img>");
+                gifImage.attr("src", results[i].images.fixed_height.url);
+
+                gifDiv.append(gifImage);
+                gifDiv.append(rate);
+
+                $("#gifs-appear-here").append(gifDiv);
+                console.log(results[i].images.fixed_height.url)
             }
-        }
-
-        $("#add-brand").on("click", function (event) {
-            event.preventDefault();
-
-            var brand = $("gif-input").val().trim();
-
-            brands.push(brand);
-
-            renderButtons();
         });
+    }
     
-        $("#add-brand").on("click", function (event) {
-            event.preventDefault();
-            parseInt($('#request-input'))
-            limit = $('#request-input').val()
-            displayGifs()
-        });
-        $(document).on("click", ".show", displayGifs);
 
+    // Function for displaying brand data
+    function renderButtons() {
+        $("#buttons-view").empty();
+        for (var i = 0; i < brands.length; i++) {
+            var a = $("<button>");
+            a.addClass("brand");
+            a.attr("data-name", brands[i]);
+            a.text(brands[i]);
+            $("#buttons-view").append(a);
+        }
+    }
+
+    // This function handles events where the add brand button is clicked
+    $("#add-brand").on("click", function (event) {
+        event.preventDefault();
+        // This line of code will grab the input from the textbox
+        var brand = $("#brand-input").val().trim();
+
+        // The brand from the textbox is then added to our array
+        brands.push(brand);
+
+        // Calling renderButtons which handles the processing of our brand array
         renderButtons();
+    });
 
-    })
+    $("#add-brand").on("click", function (event) {
+        event.preventDefault();
+        parseInt($('#request-input'))
+        limit = $('#request-input').val()
+        displayGifs()
+    });
+
+
+    // Adding click event listeners to all elements with a class of "brand"
+    $(document).on("click", ".brand", displayGifs);
+
+    // Calling the renderButtons function to display the intial buttons
+    renderButtons();
+
+});
